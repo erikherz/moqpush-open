c6828d43f70f86fc0acc36747eab540a32ec5206
Erik Herz <erikherz@Eriks-MacBook-Pro.local>
2026-03-11 08:46:38 -0400

    Install rustls crypto provider for tokio-tungstenite

--- moqpush-puller/src/main.rs
+++ moqpush-puller/src/main.rs
@@ -57,6 +57,10 @@ type ActivePulls = Arc<RwLock<HashMap<String, PullSession>>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("failed to install rustls crypto provider");
+
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
